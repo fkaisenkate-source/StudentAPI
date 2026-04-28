6e611fab5e32650a1b2f8c1017480f46581a77dc
fkaisenkate-source <fernandezkaisentkate@gmail.com>
2026-04-29 06:46:13 +0800

    Student API ready for deploy

--- obj/Debug/net10.0/StudentAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92139221e08bdea6994e379cc13640a722380918")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
